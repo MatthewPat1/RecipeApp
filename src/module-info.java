08d4fb32ab8295458564b7f39d7075ce49ebdd58
Matthew Patrus <matthew2898@hotmail.com>
2023-03-11 15:09:11 -0500

    Gradle project initialized - Recipe App

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module RecipeApp {
-	requires javafx.controls;
-	requires java.sql;
-	requires javafx.graphics;
-	requires junit;
-	exports tests to junit;
-	opens application to javafx.graphics, javafx.fxml;
-}
